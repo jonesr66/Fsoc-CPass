5ad8c8787bdd766bfffa9d9d5e6eb6e763bbff89
jonesr66 <jonesr66@uni.coventry.ac.uk>
2016-10-14 12:08:24 +0100

    Add files via upload

---
+++ Fsocpass.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+char pass
+
+printf("Please enter the passphrase:");
+scanf("%d", &pass);
+
+if (pass == "bluesky") {
+	printf("secret message!");
+}
+else if (pass != "bluesky") {
+	printf ("Wrong passphrase")
+}
+}
