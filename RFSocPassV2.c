551e52a211a8e734dbc61ac3d0855f5c36f5c758
jonesr66 <jonesr66@uni.coventry.ac.uk>
2016-10-17 14:43:56 +0100

    patch 3
    added new if statement, currently "working" but its inverted. If the pass is right you get the wrong message, vice vera - will fix in a bit.

--- RFSocPassV2.c
+++ RFSocPassV2.c
@@ -11,8 +11,9 @@ scanf("%s", user );
 printf("Please enter the passphrase:\n");
 scanf("%s", pass );
 
-if (user == "admin" && pass == "bluesky") {
-	printf ("secret message!\n");
+if (strcmp(user, "admin")) {
+	if (strcmp(pass, "bluesky")) 
+		printf ("secret message!\n");
 }
 else { 
 	printf ("Wrong passphrase.\n");
